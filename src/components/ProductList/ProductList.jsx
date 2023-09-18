6d8613ebab689739288ed2d45b931c84c45e2fd3
AwSicker <vitja1234@gmail.com>
2023-09-18 13:07:49 +0300

    bot-tele

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -40,6 +40,7 @@ const ProductList = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
+            referrerPolicy: "unsafe_url",
             body: JSON.stringify(data)
         })
     }, [addedItems])
